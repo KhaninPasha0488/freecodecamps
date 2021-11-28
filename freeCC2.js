1a4da85dd50c5c1ae15869c7a058b2d061b820b5
KhaninPasha0488 <KhaninPasha0488>
2021-11-28 13:56:25 +0300

    fCC28-2

--- freeCC2.js
+++ freeCC2.js
@@ -7,3 +7,11 @@ var titleCaseOver;
 studlyCapVar = 10;
 properCamelCase = "A String";
 titleCaseOver = 9000;
+
+let catName = "Oliver";
+let catSound = "Meow!";
+
+const FCC = "freeCodeCamp"; // Change this line
+let fact = "is cool!"; // Change this line
+fact = "is awesome!";
+console.log(FCC, fact); // Change this line
