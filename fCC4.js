6e40dce154735c57e011589aa5f8da74a27b9dc9
KhaninPasha0488 <KhaninPasha0488>
2021-12-02 12:19:00 +0300

    02-fCC11

--- fCC4.js
+++ fCC4.js
@@ -4,3 +4,58 @@ const myStr = "This is the start. " + "This is the end."; // Change this line
 
 let myStr ="This is the first sentence. ";
 myStr += "This is the second sentence.";
+
+var myFirstName = "Pavel";
+var myLastName = "Hanin";
+
+// Only change code below this line
+var a = 5;
+var b = 10;
+var c = "I am a";
+// Only change code above this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+
+// Only change code below this line
+const myName = "Pavel";
+const myStr = "My name is " + myName + " and I am well!"; 
+
+// Change code below this line
+const someAdjective = "good";
+let myStr = "Learning to code is ";
+myStr += someAdjective;
+
+// Setup
+let lastNameLength = 0;
+const lastName = "Lovelace";
+// Only change code below this line
+lastNameLength = lastName.length;
+
+// Setup
+let firstLetterOfLastName = "";
+const lastName = "Lovelace";
+
+// Only change code below this line
+firstLetterOfLastName = lastName[0]; // Change this line
+
+// Setup
+let myStr = "Jello World";
+
+// Only change code below this line
+myStr = "Hello World"; // Change this line
+// Only change code above this line
+
+// Setup
+const lastName = "Lovelace";
+
+// Only change code below this line
+const thirdLetterOfLastName = lastName[2]; // Change this line
+
+// Setup
+const lastName = "Lovelace";
+
+// Only change code below this line
+const lastLetterOfLastName = lastName[lastName.length - 1]; // Change this line
+
