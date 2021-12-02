f79e0cf20886b2f663e12af93c386e061371ab9a
KhaninPasha0488 <KhaninPasha0488>
2021-12-02 12:37:38 +0300

    02-fcc35%

--- fCC4.js
+++ fCC4.js
@@ -59,3 +59,20 @@ const lastName = "Lovelace";
 // Only change code below this line
 const lastLetterOfLastName = lastName[lastName.length - 1]; // Change this line
 
+// Setup
+const lastName = "Lovelace";
+
+// Only change code below this line
+const secondToLastLetterOfLastName = lastName[lastName.length - 2]; // Change this line
+
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+// Only change code below this line
+const wordBlanks = myAdjective + " " + myNoun + " " + myAdverb + " " + myVerb ; // Change this line
+// Only change code above this line
+
+// Only change code below this line
+const myArray = ["pavel", 2];
